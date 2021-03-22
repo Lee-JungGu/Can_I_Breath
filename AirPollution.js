06173e52e6108d0da1faf487ebb017d2ee89061d
Lee JungGu <whatmyname@naver.com>
2021-03-22 19:11:51 +0900

    미세먼지, 초미세먼지, 날씨정보 반영 및 UI 업데이트

--- AirPollution.js
+++ AirPollution.js
@@ -1,73 +1,320 @@
 import React, { useRef, useEffect } from "react";
 import { Animated, View, Text, StyleSheet } from "react-native";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function AirPollution({ airPollutionLevel, district }) {
+const airPollutionInfo = (airPollutionLevel) => {
+  if (airPollutionLevel < 21) {
+    return {
+      emoji: "smile-beam",
+      grade: "아주 좋음",
+      gradeNumber: "1",
+      baground: "#0099FF",
+      text: "매우 상쾌해요~!",
+    };
+  } else if (airPollutionLevel >= 21 && airPollutionLevel < 51) {
+    return {
+      emoji: "smile",
+      grade: "좋음",
+      gradeNumber: "2",
+      baground: "#48dbfb",
+      text: "밖에 나가기 좋아요~",
+    };
+  } else if (airPollutionLevel >= 51 && airPollutionLevel < 81) {
+    return {
+      emoji: "frown",
+      grade: "보통",
+      gradeNumber: "3",
+      baground: "#ff9f43",
+      text: "공기가 좋지는 않아요!",
+    };
+  } else if (airPollutionLevel >= 81 && airPollutionLevel < 151) {
+    return {
+      emoji: "angry",
+      grade: "나쁨",
+      gradeNumber: "4",
+      baground: "#ee5253",
+      text: "마스크 꼭 착용하세요~!",
+    };
+  } else if (airPollutionLevel >= 151 && airPollutionLevel < 251) {
+    return {
+      emoji: "skull",
+      grade: "매우 나쁨",
+      gradeNumber: "5",
+      baground: "#576574",
+      text: "집콕이 최고!!",
+    };
+  } else {
+    return {
+      emoji: "biohazard",
+      grade: "최악",
+      gradeNumber: "6",
+      baground: "#222f3e",
+      text: "숨쉬기 힘들어요!!!!!",
+    };
+  }
+};
+
+const microAirPollutionInfo = (microAirPollutionLevel) => {
+  if (microAirPollutionLevel < 9) {
+    return {
+      emoji: "smile-beam",
+      grade: "아주 좋음",
+      gradeNumber: "1",
+      baground: "#0099FF",
+      text: "매우 상쾌해요~!",
+    };
+  } else if (microAirPollutionLevel >= 9 && microAirPollutionLevel < 16) {
+    return {
+      emoji: "smile",
+      grade: "좋음",
+      gradeNumber: "2",
+      baground: "#48dbfb",
+      text: "밖에 나가기 좋아요~",
+    };
+  } else if (microAirPollutionLevel >= 16 && microAirPollutionLevel < 51) {
+    return {
+      emoji: "frown",
+      grade: "보통",
+      gradeNumber: "3",
+      baground: "#ff9f43",
+      text: "공기가 좋지는 않아요!",
+    };
+  } else if (microAirPollutionLevel >= 51 && microAirPollutionLevel < 101) {
+    return {
+      emoji: "angry",
+      grade: "나쁨",
+      gradeNumber: "4",
+      baground: "#ee5253",
+      text: "마스크 꼭 착용하세요~!",
+    };
+  } else if (microAirPollutionLevel >= 101 && microAirPollutionLevel < 151) {
+    return {
+      emoji: "skull",
+      grade: "매우 나쁨",
+      gradeNumber: "5",
+      baground: "#576574",
+      text: "집콕이 최고!!",
+    };
+  } else {
+    return {
+      emoji: "biohazard",
+      grade: "최악",
+      gradeNumber: "6",
+      baground: "#222f3e",
+      text: "숨쉬기 힘들어요!!!!!",
+    };
+  }
+};
+
+const weatherInfo = (weather) => {
+  if (weather === 4201) {
+    return {
+      weather: "폭우",
+      emoji: "weather-pouring",
+    };
+  } else if (weather === 4001) {
+    return {
+      weather: "비",
+      emoji: "weather-rainy",
+    };
+  } else if (weather === 4200) {
+    return {
+      weather: "약한 비",
+      emoji: "weather-rainy",
+    };
+  } else if (weather === 4000) {
+    return {
+      weather: "약한 비",
+      emoji: "weather-rainy",
+    };
+  } else if (weather === 6201) {
+    return {
+      weather: "폭우(빙우)",
+      emoji: "weather-snowy-rainy",
+    };
+  } else if (weather === 6001) {
+    return {
+      weather: "비(빙우)",
+      emoji: "weather-snowy-rainy",
+    };
+  } else if (weather === 6200) {
+    return {
+      weather: "약한 비(빙우)",
+      emoji: "weather-snowy-rainy",
+    };
+  } else if (weather === 6000) {
+    return {
+      weather: "약한 비(빙우)",
+      emoji: "weather-snowy-rainy",
+    };
+  } else if (weather === 7101) {
+    return {
+      weather: "우박(강함)",
+      emoji: "weather-hail",
+    };
+  } else if (weather === 7000) {
+    return {
+      weather: "우박",
+      emoji: "weather-hail",
+    };
+  } else if (weather === 7102) {
+    return {
+      weather: "우박(약함)",
+      emoji: "weather-hail",
+    };
+  } else if (weather === 5101) {
+    return {
+      weather: "폭설",
+      emoji: "weather-snowy-heavy",
+    };
+  } else if (weather === 5000) {
+    return {
+      weather: "눈",
+      emoji: "weather-snowy",
+    };
+  } else if (weather === 5100) {
+    return {
+      weather: "약한 눈",
+      emoji: "weather-snowy",
+    };
+  } else if (weather === 5001) {
+    return {
+      weather: "돌풍",
+      emoji: "weather-windy-variant",
+    };
+  } else if (weather === 8000) {
+    return {
+      weather: "뇌우",
+      emoji: "weather-lightning",
+    };
+  } else if (weather === 2100) {
+    return {
+      weather: "약한 안개",
+      emoji: "weather-fog",
+    };
+  } else if (weather === 2000) {
+    return {
+      weather: "안개",
+      emoji: "weather-fog",
+    };
+  } else if (weather === 1001) {
+    return {
+      weather: "흐림",
+      emoji: "weather-cloudy",
+    };
+  } else if (weather === 1102) {
+    return {
+      weather: "대체로 흐림",
+      emoji: "weather-cloudy",
+    };
+  } else if (weather === 1101) {
+    return {
+      weather: "구름 조금",
+      emoji: "weather-partly-cloudy",
+    };
+  } else if (weather === 1100) {
+    return {
+      weather: "대체로 맑음",
+      emoji: "weather-sunny",
+    };
+  } else if (weather === 1000) {
+    return {
+      weather: "맑음",
+      emoji: "weather-sunny",
+    };
+  }
+};
+
+export default function AirPollution({
+  airPollutionLevel,
+  microAirPollutionLevel,
+  district,
+  temperature,
+  weather,
+}) {
   const emojiAnim = useRef(new Animated.ValueXY({ x: 0, y: 30 })).current;
-
-  const airInfo = () => {
-    if (airPollutionLevel < 20) {
-      return {
-        emoji: "smile-beam",
-        baground: "#0099FF",
-        text: "The air is very clean, so if you don't go out, you lose!!",
-      };
-    } else if (airPollutionLevel >= 20 && airPollutionLevel < 50) {
-      return {
-        emoji: "smile",
-        baground: "#48dbfb",
-        text: "The air is clean, so it's okay to go out :)",
-      };
-    } else if (airPollutionLevel >= 50 && airPollutionLevel < 80) {
-      return {
-        emoji: "frown",
-        baground: "#ff9f43",
-        text: "The air is not so good, don't stay outside for a long time :(",
-      };
-    } else if (airPollutionLevel >= 80 && airPollutionLevel < 100) {
-      return {
-        emoji: "angry",
-        baground: "#ee5253",
-        text: "The air is bad, so be sure to wear a mask to get out!",
-      };
-    } else if (airPollutionLevel >= 100 && airPollutionLevel < 150) {
-      return {
-        emoji: "skull",
-        baground: "#576574",
-        text: "The air is very bad, so it’s better not to go out...",
-      };
-    } else {
-      return {
-        emoji: "biohazard",
-        baground: "#222f3e",
-        text:
-          "The air is the fuck. Do you want to go outside? Yes it's your life :)",
-      };
-    }
-  };
-
   const animateEmoji = Animated.spring(emojiAnim, {
     toValue: { x: 0, y: 0 },
     friction: 2,
     tension: 100,
     useNativeDriver: true,
   });
+  const comparePollutionLevel = () => {
+    if (
+      airPollutionInfo(airPollutionLevel).gradeNumber >
+      microAirPollutionInfo(microAirPollutionLevel).gradeNumber
+    )
+      return airPollutionInfo(airPollutionLevel);
+    else return microAirPollutionInfo(microAirPollutionLevel);
+  };
 
   useEffect(() => {
     animateEmoji.start();
   });
 
   return (
-    <View style={[styles.container, { backgroundColor: airInfo().baground }]}>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: comparePollutionLevel().baground },
+      ]}
+    >
       <View style={styles.mainContent}>
+        <Text style={styles.district}>({district})</Text>
         <Animated.View style={{ transform: [{ translateY: emojiAnim.y }] }}>
-          <FontAwesome5 name={airInfo().emoji} style={styles.emoji} />
+          <FontAwesome5
+            name={comparePollutionLevel().emoji}
+            style={styles.mainEmoji}
+          />
         </Animated.View>
-        <Text style={styles.airPollutionLevel}>{airPollutionLevel}</Text>
-        <Text style={styles.district}>{district}</Text>
+        <Text style={styles.airPollutionLevel}>
+          {comparePollutionLevel().grade}
+        </Text>
       </View>
-      <View style={styles.subContent}>
-        <Text style={styles.comment}>{airInfo().text}</Text>
+      <View style={styles.subText}>
+        <Text style={styles.comment}>{comparePollutionLevel().text}</Text>
+      </View>
+      <View style={styles.weatherContent}>
+        <View
+          style={[
+            styles.weatherBox,
+            { backgroundColor: comparePollutionLevel().baground },
+          ]}
+        >
+          <View style={styles.weatherBoxContent}>
+            <Text style={styles.weatherBoxTitle}>미세먼지</Text>
+            <FontAwesome5
+              name={airPollutionInfo(airPollutionLevel).emoji}
+              style={styles.weatherBoxEmoji}
+            />
+            <Text style={styles.weatherBoxGrade}>
+              {airPollutionInfo(airPollutionLevel).grade}
+            </Text>
+            <Text style={styles.weatherBoxLevel}>{airPollutionLevel}</Text>
+          </View>
+          <View style={styles.weatherBoxContent}>
+            <Text style={styles.weatherBoxTitle}>초미세먼지</Text>
+            <FontAwesome5
+              name={microAirPollutionInfo(microAirPollutionLevel).emoji}
+              style={styles.weatherBoxEmoji}
+            />
+            <Text style={styles.weatherBoxGrade}>
+              {microAirPollutionInfo(microAirPollutionLevel).grade}
+            </Text>
+            <Text style={styles.weatherBoxLevel}>{microAirPollutionLevel}</Text>
+          </View>
+          <View style={[styles.weatherBoxContent, { borderRightWidth: 0 }]}>
+            <Text style={styles.weatherBoxTitle}>날씨</Text>
+            <MaterialCommunityIcons
+              name={weatherInfo(weather).emoji}
+              style={styles.weatherBoxEmoji}
+            />
+            <Text style={styles.weatherBoxGrade}>
+              {weatherInfo(weather).weather}
+            </Text>
+            <Text style={styles.weatherBoxLevel}>{temperature}º</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -79,27 +326,28 @@ const styles = StyleSheet.create({
   },
 
   mainContent: {
-    flex: 2,
-    justifyContent: "center",
+    flex: 3,
+    justifyContent: "flex-end",
     alignItems: "center",
-  },
-  emoji: {
-    fontSize: 120,
-    marginBottom: 20,
-    color: "white",
-  },
-  airPollutionLevel: {
-    fontSize: 40,
-    marginBottom: 20,
-    color: "white",
   },
   district: {
     fontSize: 20,
     color: "white",
+    marginBottom: 20,
+  },
+  mainEmoji: {
+    fontSize: 120,
+    color: "white",
+    marginBottom: 20,
+  },
+  airPollutionLevel: {
+    fontSize: 40,
+    color: "white",
   },
 
-  subContent: {
-    flex: 1,
+  subText: {
+    flex: 2,
+    justifyContent: "center",
     alignItems: "center",
   },
   comment: {
@@ -107,5 +355,54 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: "white",
     lineHeight: 30,
+  },
+
+  weatherContent: {
+    flex: 3,
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+  weatherBox: {
+    flex: 0.5,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "84%",
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: "white",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 1,
+      height: 2,
+    },
+    shadowOpacity: 0.5,
+    shadowColor: "#2c2c2c",
+  },
+  weatherBoxContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRightWidth: 1,
+    borderColor: "white",
+  },
+  weatherBoxTitle: {
+    fontSize: 16,
+    color: "white",
+    marginBottom: 12,
+  },
+  weatherBoxEmoji: {
+    fontSize: 40,
+    color: "white",
+    marginBottom: 8,
+  },
+  weatherBoxGrade: {
+    fontSize: 15,
+    color: "white",
+    marginBottom: 8,
+  },
+  weatherBoxLevel: {
+    fontSize: 15,
+    color: "white",
   },
 });
--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Alert } from "react-native";
+import { Alert } from "react-native";
 import * as Location from "expo-location";
 import axios from "axios";
 import Loading from "./Loading";
@@ -8,54 +7,79 @@ import AirPollution from "./AirPollution";
 
 const API_KEY =
   "R2eFd8GtX8RsVffMn9QKamgFYKlgcEzlGKBImqIWQ9No60kJerbuzqp8Vn%2B44DyxEEVmrlhJyo9yNbQiE1KjFQ%3D%3D";
+const SECRET_KEY = "BoOadcth2YH5h8XP3x5o7XGOd3Bwr42O";
 
 export default class App extends React.Component {
   state = {
     isLoading: true,
     airPollutionLevel: null,
+    microAirPollutionLevel: null,
     district: null,
+    temperature: null,
+    weather: null,
   };
 
-  getAirInfo = async (cityName, district) => {
+  getWeather = async (latitude, longitude) => {
     try {
-      const airInfo = await axios(
-        `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=${API_KEY}&returnType=json&numOfRows=200&pageNo=1&sidoName=${cityName}&ver=1.0`
+      const wheather = await axios(
+        `https://data.climacell.co/v4/timelines?location=${latitude},${longitude}&fields=temperature,weatherCode,particulateMatter25,particulateMatter10&timesteps=current&units=metric&apikey=${SECRET_KEY}`
       );
       const {
         data: {
-          response: {
-            body: { items },
+          data: {
+            timelines: [
+              {
+                intervals: [
+                  {
+                    values: {
+                      temperature,
+                      weatherCode,
+                      particulateMatter25,
+                      particulateMatter10,
+                    },
+                  },
+                ],
+              },
+            ],
           },
         },
-      } = airInfo;
+      } = wheather;
 
-      let stationName = [];
-      let airPollutionLevel = 0;
+      // if (weatherCode === 4201) this.setState({ weather: "폭우" });
+      // else if (weatherCode === 4001) this.setState({ weather: "비" });
+      // else if (weatherCode === 4200) this.setState({ weather: "소우" });
+      // else if (weatherCode === 4000) this.setState({ weather: "소우" });
+      // else if (weatherCode === 6201) this.setState({ weather: "폭우(빙우)" });
+      // else if (weatherCode === 6001) this.setState({ weather: "비(빙우)" });
+      // else if (weatherCode === 6200) this.setState({ weather: "소우(빙우)" });
+      // else if (weatherCode === 6000) this.setState({ weather: "이슬비(빙우)" });
+      // else if (weatherCode === 7101) this.setState({ weather: "우박(강함)" });
+      // else if (weatherCode === 7000) this.setState({ weather: "우박" });
+      // else if (weatherCode === 7102) this.setState({ weather: "우박(약함)" });
+      // else if (weatherCode === 5101) this.setState({ weather: "폭설" });
+      // else if (weatherCode === 5000) this.setState({ weather: "눈" });
+      // else if (weatherCode === 5100) this.setState({ weather: "약한 눈" });
+      // else if (weatherCode === 5001) this.setState({ weather: "소나기" });
+      // else if (weatherCode === 8000) this.setState({ weather: "뇌우" });
+      // else if (weatherCode === 2100) this.setState({ weather: "약한 안개" });
+      // else if (weatherCode === 2000) this.setState({ weather: "안개" });
+      // else if (weatherCode === 1001) this.setState({ weather: "흐림" });
+      // else if (weatherCode === 1102) this.setState({ weather: "대체로 흐림" });
+      // else if (weatherCode === 1101) this.setState({ weather: "구름 조금" });
+      // else if (weatherCode === 1100) this.setState({ weather: "대체로 맑음" });
+      // else if (weatherCode === 1000) this.setState({ weather: "맑음" });
 
-      for (const item of items) {
-        stationName.push(item.stationName);
-      }
-
-      if (stationName.includes(district)) {
-        const indexOfCity = stationName.indexOf(district);
-        airPollutionLevel = items[indexOfCity].pm10Value;
-      } else {
-        airPollutionLevel = items[0].pm10Value;
-      }
-
-      this.setState({ airPollutionLevel });
-
-      if (district != null) {
-        this.setState({ district });
-      } else {
-        this.setState({ district: "현재 위치" });
-      }
-
-      this.setState({ isLoading: false });
+      this.setState({
+        weather: weatherCode,
+        temperature: Math.round(temperature),
+        airPollutionLevel: particulateMatter10,
+        microAirPollutionLevel: particulateMatter25,
+        isLoading: false,
+      });
     } catch {
       Alert.alert(
-        "Could you please approve the location information?",
-        "Please..."
+        "날씨정보를 불러오는 중 에러가 발생하였습니다.",
+        "앱을 다시 실행해 주세요."
       );
     }
   };
@@ -63,18 +87,21 @@ export default class App extends React.Component {
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
+
       const location = await Location.getCurrentPositionAsync();
       const {
         coords: { latitude, longitude },
       } = location;
       const city = await Location.reverseGeocodeAsync({ latitude, longitude });
-      const { region, district } = city[0];
-      const cityName = encodeURI(region.slice(0, 2));
-      this.getAirInfo(cityName, district);
+      const { district } = city[0];
+      if (district != null) this.setState({ district });
+      else this.setState({ district: "현재 위치" });
+
+      this.getWeather(latitude, longitude);
     } catch {
       Alert.alert(
-        "Could you please approve the location information?",
-        "Please..."
+        "위치정보 승인이 필요합니다.",
+        "설정에서 위치정보 승인해 주세요."
       );
     }
   };
@@ -84,11 +111,24 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { isLoading, airPollutionLevel, district } = this.state;
+    const {
+      isLoading,
+      airPollutionLevel,
+      microAirPollutionLevel,
+      district,
+      temperature,
+      weather,
+    } = this.state;
     return isLoading ? (
       <Loading />
     ) : (
-      <AirPollution airPollutionLevel={airPollutionLevel} district={district} />
+      <AirPollution
+        airPollutionLevel={airPollutionLevel}
+        microAirPollutionLevel={microAirPollutionLevel}
+        district={district}
+        temperature={temperature}
+        weather={weather}
+      />
     );
   }
 }
