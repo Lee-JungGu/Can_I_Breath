e33d6e57076687d9e18ef75efa908932c3a84cee
Lee JungGu <whatmyname@naver.com>
2021-03-24 17:13:00 +0900

    위치 API를 바꿔 어플리케이션 속도 개선 완료

--- src/App.js
+++ src/App.js
@@ -15,10 +15,9 @@ export default class App extends React.Component {
     weather: "",
   };
 
-  getWeather = async () => {
+  getWeather = async (latitude, longitude) => {
     try {
       const API_KEY = "BoOadcth2YH5h8XP3x5o7XGOd3Bwr42O";
-      const { latitude, longitude } = await this.getLocation();
       const wheather = await axios(
         `https://data.climacell.co/v4/timelines?location=${latitude},${longitude}&fields=temperature,weatherCode,particulateMatter25,particulateMatter10&timesteps=current&units=metric&apikey=${API_KEY}`
       );
@@ -42,7 +41,6 @@ export default class App extends React.Component {
           },
         },
       } = wheather;
-
       this.setState({
         weather: weatherCode,
         temperature: Math.round(temperature),
@@ -58,35 +56,26 @@ export default class App extends React.Component {
     }
   };
 
-  getCity = async () => {
-    try {
-      const { latitude, longitude } = await this.getLocation();
-      const city = await Location.reverseGeocodeAsync({ latitude, longitude });
-      const [{ city: cityName, district }] = city;
+  getCity = async (latitude, longitude) => {
+    const city = await Location.reverseGeocodeAsync({ latitude, longitude });
+    const [{ district, city: cityName }] = city;
 
-      this.checkDistrict(district, cityName);
-    } catch {}
-  };
-
-  checkDistrict = (district, cityName) => {
-    if (district != null) this.setState({ district });
-    else if (district === null && cityName != null)
-      this.setState({ district: cityName });
-    else this.setState({ district: "현재 위치" });
+    if (district != null) return this.setState({ district });
+    if (district === null && cityName != null)
+      return this.setState({ district: cityName });
+    return this.setState({ district: "현재 위치" });
   };
 
   getLocation = async () => {
     try {
-      await Location.requestPermissionsAsync();
+      navigator.geolocation.getCurrentPosition((position) => {
+        const {
+          coords: { latitude, longitude },
+        } = position;
 
-      const location = await Location.getCurrentPositionAsync();
-      const {
-        coords: { latitude, longitude },
-      } = location;
-
-      // this.getCity(latitude, longitude);
-      // this.getWeather(latitude, longitude);
-      return { latitude, longitude };
+        this.getCity(latitude, longitude);
+        this.getWeather(latitude, longitude);
+      });
     } catch {
       Alert.alert(
         "위치정보 승인이 필요합니다.",
@@ -96,9 +85,7 @@ export default class App extends React.Component {
   };
 
   componentDidMount() {
-    // this.getLocation();
-    this.getCity();
-    this.getWeather();
+    this.getLocation();
   }
 
   render() {
